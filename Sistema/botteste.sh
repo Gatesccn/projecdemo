f70025d9b06a48504ee64121c4ab79fc09b1a893
Gatesccn <99981713+Gatesccn@users.noreply.github.com>
2022-05-20 02:15:11 -0400

    Update botteste.sh

--- Sistema/botteste.sh
+++ Sistema/botteste.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 [[ $(screen -list| grep -c 'bot_teste') == '0' ]] && {
-    echo  -e "Imforme o token"
-    read token
+    clear
+    echo -e "\E[44;1;37m     ACTIVANDO BOT SSH TESTE     \E[0m"
+    echo ""
+    echo -ne "\n\033[1;32mTOKEN\033[1;37m: "; read token
     cd $HOME/BOT
     screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
+    clear && echo "BOT ATIVADO"
 } || {
     screen -r -S "bot_teste" -X quit
     clear && echo "BOT DESATIVADO"
